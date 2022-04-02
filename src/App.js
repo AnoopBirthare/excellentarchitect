c305f42e86662f9fb5029a3523284b8272a0a4af
Anoop <anoopbirthare21@gmail.com>
2022-04-02 20:43:30 +0530

    carousel

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 import CarouselComp from './components/Carousle';
 import Header from './components/Header';
 
 
 function App() {
+  console.log("window-->",window.innerWidth)
   return (
-    <div className="App">      
-<Header />
-      <CarouselComp style={{zIndex:1,position:"absolute"}}/>
+    <div className="App" style={{flex:1}}>
+      <div style={{position:window.innerWidth>1000?"fixed":"static",zIndex:2,width:"100%",backgroundColor:"rgba(65, 79, 91,.90)"}}>      
+      <Header/>
+      </div>
+      <div style={{}}>
+      <CarouselComp/>
+      </div>
+
+
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,22 @@
 import React from "react";
+import Logo from "../assets/exelogo.png"
 import {Navbar, Nav,Container, NavDropdown} from 'react-bootstrap';
 
 const Header =()=>{
 
     return(
-        <Navbar sticky="top" collapseOnSelect expand="lg" bg="transparent" variant="light">
+        <Navbar  collapseOnSelect expand="lg" bg="transparent" variant="dark">
   <Container>
+  <img
+     style={{flex: 1,
+      resizeMode: 'contain'}}
+      src={Logo}
+    />
   <Navbar.Brand href="#home">Excellent Architect</Navbar.Brand>
   <Navbar.Toggle aria-controls="responsive-navbar-nav" />
   <Navbar.Collapse id="responsive-navbar-nav">
-    <Nav className="me-auto">
-      <Nav.Link href="#features">Features</Nav.Link>
+    <Nav className="me-auto" style={{justifyContent:"center",alignItems:"center"}}>
+      <Nav.Link href="#features" >Features</Nav.Link>
       <Nav.Link href="#pricing">Pricing</Nav.Link>
       <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
         <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
