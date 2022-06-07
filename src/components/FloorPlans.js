6e88d57fd6708ee6a420a2d90c40d47e8adcbb4a
Anoop <anoopbirthare21@gmail.com>
2022-06-08 00:23:03 +0530

    changed all elevation images

--- src/components/FloorPlans.js
+++ src/components/FloorPlans.js
@@ -4,14 +4,11 @@
  import Caro1 from "../assets/Caro1.png";
  import Caro2 from "../assets/Caro2.png";
  import Caro3 from "../assets/Caro3.png";
- import Caro4 from "../assets/Caro4.png";
- import Caro5 from "../assets/Caro5.png";
- import Caro6 from "../assets/Caro6.png";
- import Caro7 from "../assets/Caro7.png";
- import Caro8 from "../assets/Caro8.png";
- import Caro9 from "../assets/Caro9.png";
- import Caro10 from "../assets/Caro10.png";
- 
+ import Caro4 from "../assets/Caro4.jpg";
+  import Caro5 from "../assets/Caro5.png";
+  import Caro6 from "../assets/Caro6.jpg";
+  import Caro7 from "../assets/Caro7.png";
+  import Caro8 from "../assets/Caro8.png";
 const FloorPlans = ()=> {
 
        return (
@@ -33,25 +30,25 @@ const FloorPlans = ()=> {
           <img class="gallery-image" src={Caro3} alt="Your Dream Design is Loading"/>
         </div>
       
-        <div class="gallery-item">
+         <div class="gallery-item">
           <img class="gallery-image" src={Caro4} alt="Your Dream Design is Loading"/>
         </div>
       
-        <div class="gallery-item">
+      <div class="gallery-item">
           <img class="gallery-image" src={Caro5} alt="Your Dream Design is Loading"/>
         </div>
       
-        <div class="gallery-item">
+         <div class="gallery-item">
           <img class="gallery-image" src={Caro6} alt="Your Dream Design is Loading"/>
         </div>
 
-        <div class="gallery-item">
+         <div class="gallery-item">
           <img class="gallery-image" src={Caro7} alt="Your Dream Design is Loading"/>
         </div>
       
-        <div class="gallery-item">
+       <div class="gallery-item">
           <img class="gallery-image" src={Caro8} alt="Your Dream Design is Loading"/>
-        </div>
+        </div> 
       
       </div>
       
